af4f4d11887f9cc231b1fe2419ccd1ca198cda89
bhavya vineet <bhavya07vineet@gmail.com>
2024-06-15 19:19:41 -0700

    support piping

--- main.go
+++ main.go
@@ -20,42 +20,56 @@ func main() {
 		}
 
 		input = strings.TrimSpace(input)
-		parts := strings.Split(input, " ")
-		command := parts[0]
-		parts = parts[1:]
+		piped_commands := strings.Split(input, "|")
+		temp_output := ""
 
-		switch command {
-		
-		case "exit":
-			os.Exit(0)
-
-		case "cd":
-			err = os.Chdir(parts[0])
-			if err != nil {
-				fmt.Println("Error in changing directory:", err)
+		for i := 0; i < len(piped_commands); i++ {
+			temp_input := strings.TrimSpace(piped_commands[i])
+			parts := strings.Split(temp_input, " ")
+			command := parts[0]
+			parts = parts[1:]
+			if (temp_output) != "" {
+				parts = append(parts,temp_output)
 			}
 
-			continue
-
-		case "pwd" :
-			dir, err := os.Getwd()
-			if err != nil {
-				fmt.Println("Error in changing directory:", err)
-			}
-			fmt.Println("Current working directory:", dir)
-			continue		
+			switch command {
 			
-		default:
+			case "exit":
+				os.Exit(0)
 
-			cmd := exec.Command(command,parts...)
-			output, err := cmd.Output()
+			case "cd":
+				err = os.Chdir(parts[0])
+				if err != nil {
+					fmt.Println("Error in changing directory:", err)
+					continue
+				}
 
-			if err != nil {
-				fmt.Println("Error executing command:", err)
-				continue
+				
+
+			case "pwd":
+				output, err := os.Getwd()
+				if err != nil {
+					fmt.Println("Error in changing directory:", err)
+					continue
+				}
+				temp_output = string(output)
+						
+				
+			default:
+				cmd := exec.Command(command,parts...)
+				output, err := cmd.Output()
+
+				if err != nil {
+					fmt.Println("Error executing command:", err)
+					continue
+				}
+				temp_output = string(output)
 			}
-
-		// Print the output
-		fmt.Println(string(output))}
+				
+			
+		}
+		if temp_output != "" {
+			fmt.Println(temp_output)
+		}
 	}
 }
